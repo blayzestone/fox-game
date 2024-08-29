a7f0aecdc0ccf6fff6562bb1cb1d1b0689bb805c
Blayze Stone <blayzeostone@gmail.com>
2024-08-28 21:37:44 -0400

    ui interaction implemented

--- src/init.js
+++ src/init.js
@@ -1,9 +1,10 @@
-import gameState from "./gameState";
-
-const TICK_RATE = 3000
+import initButtons from "./buttons";
+import { TICK_RATE } from "./constants";
+import game from "./gameState";
 
 function init() {
   console.log("starting game");
+  initButtons(game.handleUserAction)
 
   let nextTimeToTick = Date.now();
 
@@ -11,7 +12,7 @@ function init() {
     const now = Date.now();
 
     if (nextTimeToTick <= now) {
-      gameState.tick();
+      game.tick();
       nextTimeToTick = now + TICK_RATE
     }
 
